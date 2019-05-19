54c7ed76d14bf0e067f425d5a1bc5a87594045dd
AHTOH668 <50781925+AHTOH668@users.noreply.github.com>
2019-05-19 15:45:44 +0300

    Добавлен метод area

---
+++ Sandbox/src/main/java/m1/pack/Point.java
@@ -0,0 +1,7 @@
+package m1.pack;
+
+public class Point {
+
+    public double x;
+    public double y;
+}
---
+++ Sandbox/src/main/java/pack/helllo/sandbox/Hello.java
@@ -0,0 +1,9 @@
+package pack.helllo.sandbox;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("pack.helllo.sandbox.Hello, world!");
+    }
+
+}
