26380bfda4944892b409935a0f43f5c516fe66a2
AHTOH668 <50781925+AHTOH668@users.noreply.github.com>
2019-05-19 18:18:53 +0300

    Плохая попытка перенести функцию в класс

--- Sandbox/src/main/java/m1/pack/Main.java
+++ Sandbox/src/main/java/m1/pack/Main.java
@@ -5,17 +5,11 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Point p1 = new Point();
-        p1.x = 10;
-        p1.y = 1;
+        Point p1 = new Point(10, 1);
 
-        Point p2 = new Point();
-        p2.x = 1;
-        p2.y = 1;
+        Point p2 = new Point(1,1);
 
         System.out.println(area(p1, p2));
     }
-    public static double area(Point p1, Point p2) {
-        return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
-    }
+
 }
---
+++ Sandbox/src/test/java/m1/pack/PointTtest.java
@@ -0,0 +1,12 @@
+package m1.pack;
+
+public class PointTtest {
+
+        public void testArea() {
+
+        Point t = new Point();
+        t.x = 10;
+        t.y = 1;
+
+    }
+}
