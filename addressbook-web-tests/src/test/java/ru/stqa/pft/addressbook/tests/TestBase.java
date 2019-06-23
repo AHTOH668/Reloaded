d1a056a921217ba43bdf9e1d1f77055996a160a0
AHTOH668 <50781925+AHTOH668@users.noreply.github.com>
2019-06-23 20:39:09 +0300

    Теперь тест бейс сломался

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -4,13 +4,22 @@ import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appManager.ApplicationManager;
+import java.io.IOException;
 
 public class TestBase {
 
-    protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+    protected static ApplicationManager app;
+
+    static {
+        try {
+            app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @BeforeSuite(alwaysRun = true)
-    public void setUp() {
+    public void setUp() throws IOException {
         app.init();
     }
 
